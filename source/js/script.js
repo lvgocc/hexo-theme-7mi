8556c3e2d2dc0fee7c8bab51551eec1bfb810013
lvgo <lvgorice@gmail.com>
2020-10-20 19:22:09 +0800

    更新博客

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,3 @@
-
 (function ($) {
   console.log('© Theme-Vexo | https://github.com/yanm1ng/hexo-theme-vexo')
   var app = $('.app-body')
@@ -104,9 +103,42 @@
     }
   })
 
-  function gitalkMd5(path){
-    const str = this.url_for(path);
-    return crypto.createHash('md5').update(str).digest('hex');
-  }
 
-})(jQuery)
+
+  // wrapImageWithFancyBox();
+})(jQuery);
+/**
+ * Wrap images with fancybox support.
+ */
+function wrapImageWithFancyBox() {
+  $('img').not('.sidebar-image img').not('#author-avatar img').not(".mdl-menu img").not(".something-else-logo img").each(function() {
+    var $image = $(this);
+    var imageCaption = $image.attr('alt');
+    var $imageWrapLink = $image.parent('a');
+
+    if ($imageWrapLink.size() < 1) {
+      var src = this.getAttribute('src');
+      var idx = src.lastIndexOf('?');
+      if (idx != -1) {
+        src = src.substring(0, idx);
+      }
+      $imageWrapLink = $image.wrap('<a href="' + src + '"></a>').parent('a');
+    }
+
+    $imageWrapLink.attr('data-fancybox', 'images');
+    if (imageCaption) {
+      $imageWrapLink.attr('data-caption', imageCaption);
+    }
+
+  });
+
+  $().fancybox({
+    selector: '[data-fancybox="images"]',
+    thumbs: false,
+    hash: true,
+    loop: false,
+    fullScreen: false,
+    slideShow: false,
+    protect: true,
+  });
+}
